4a7520c36b091e5dee9b33aeb415b4dff95b7976
mc77 <marcos.mondejar71@gmail.com>
2026-07-24 22:03:26 +0200

    chore: auto-update 2026-07-24 22:03

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -71,7 +71,7 @@ function y
     set tmp (mktemp -t "yazi-cwd.XXXXX")
     yazi $argv --cwd-file="$tmp"
     if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        cd "$cwd"
+        builtin cd "$cwd"
     end
     rm -f -- "$tmp"
 end
